e239c6b1bb17f686adf83e35af745385a655e56f
Akash Pratap Dev <akashdev1921@gmail.com>
2026-08-04 16:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,6 @@ st.set_page_config(
 )
 
 # ---------- Sidebar ----------
-with st.sidebar:
-    st.image('https://cdn-icons-png.flaticon.com/512/3062/3062634.png', width=120)
-  
-
-    st.write('**Developer:** Akash Pratap Dev')
-    st.write('**Course:** B.Tech CSE')
-    st.write('**Project:** Titanic Survival Prediction')
 
 
 
